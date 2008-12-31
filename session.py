ff072b57e38f3d592577b6d4465b731dfbfa34dd
Matt Goodall <matt.goodall@gmail.com>
2008-12-31 12:08:29 +0000

    Combine into a single module until I can be bothered to package things better.

--- session.py
+++ session.py
@@ -1,8 +1,8 @@
 import copy
 import itertools
 import couchdb
-
-from proxy import wrap
+import UserDict
+from peak.util.proxies import ObjectWrapper
 
 
 class Session(object):
@@ -163,6 +163,100 @@ class SessionRow(object):
                 return cached
             return self._session._cached(doc)
     doc = property(_get_doc)
+
+
+def wrap(obj, modified):
+    if isinstance(obj, couchdb.Document):
+        return Document(obj, modified)
+    elif isinstance(obj, dict):
+        return Dictionary(obj, modified)
+    elif isinstance(obj, list):
+        return List(obj, modified)
+    return obj
+
+
+class Dictionary(UserDict.DictMixin, ObjectWrapper):
+
+    # TODO:
+    #   __contains__(), __iter__(), and iteritems() to improve performance
+
+    __modified = None
+
+    def __init__(self, subject, modified):
+        super(Dictionary, self).__init__(subject)
+        self.__modified = modified
+
+    def __getitem__(self, *a, **k):
+        value = self.__subject__.__getitem__(*a, **k)
+        return wrap(value, self.__modified)
+
+    def __setitem__(self, *a, **k):
+        self.__modified()
+        return self.__subject__.__setitem__(*a, **k)
+
+    def __delitem__(self, *a, **k):
+        self.__modified()
+        return self.__subject__.__delitem__(*a, **k)
+
+    def keys(self, *a, **k):
+        return self.__subject__.keys(*a, **k)
+
+
+class Document(Dictionary):
+    pass
+
+
+class List(ObjectWrapper):
+
+    __modified = None
+
+    def __init__(self, subject, modified):
+        super(List, self).__init__(subject)
+        self.__modified = modified
+
+    def __getitem__(self, *a, **k):
+        value = self.__subject__.__getitem__(*a, **k)
+        return wrap(value, self.__modified)
+        
+    def __getslice__(self, *a, **k):
+        value = self.__subject__.__getslice__(*a, **k)
+        return wrap(value, self.__modified)
+        
+    def __setitem__(self, *a, **k):
+        self.__modified()
+        return self.__subject__.__setitem__(*a, **k)
+
+    def __delitem__(self, *a, **k):
+        self.__modified()
+        return self.__subject__.__delitem__(*a, **k)
+
+    def __setslice__(self, *a, **k):
+        self.__modified()
+        return self.__subject__.__setslice__(*a, **k)
+
+    def __delslice__(self, *a, **k):
+        self.__modified()
+        return self.__subject__.__delslice__(*a, **k)
+
+    def append(self, *a, **k):
+        self.__modified()
+        return self.__subject__.append(*a, **k)
+
+    def extend(self, *a, **k):
+        self.__modified()
+        return self.__subject__.extend(*a, **k)
+
+    def insert(self, *a, **k):
+        self.__modified()
+        return self.__subject__.insert(*a, **k)
+
+    def pop(self, *a, **k):
+        self.__modified()
+        return self.__subject__.pop(*a, **k)
+
+    def remove(self, *a, **k):
+        self.__modified()
+        return self.__subject__.remove(*a, **k)
 
 
 if __name__ == '__main__':
